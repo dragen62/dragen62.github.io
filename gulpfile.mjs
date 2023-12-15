88a47d9bf8f7bdc2c4d529fd14577f1cf7a16a80
dragen62 <125238620+dragen62@users.noreply.github.com>
2023-12-15 16:33:51 +0700

    refactored gulp config

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,22 +1,28 @@
 import gulp from "gulp";
+
 import del from "del";
 import include from "gulp-file-include";
 import formatHtml from "gulp-format-html";
+
 import less from "gulp-less";
 import plumber from "gulp-plumber";
 import postcss from "gulp-postcss";
 import autoprefixer from "autoprefixer";
+
 import sortMediaQueries from "postcss-sort-media-queries";
 import minify from "gulp-csso";
 import rename from "gulp-rename";
 import terser from "gulp-terser";
+
 import imagemin from "gulp-imagemin";
 import imagemin_gifsicle from "imagemin-gifsicle";
 import imagemin_mozjpeg from "imagemin-mozjpeg";
 import imagemin_optipng from "imagemin-optipng";
 import svgmin from "gulp-svgmin";
 import svgstore from "gulp-svgstore";
+
 import server from "browser-sync";
+
 const resources = {
   html: "src/html/**/*.html",
   jsDev: "src/scripts/dev/**/*.js",
@@ -34,10 +40,12 @@ const resources = {
     "src/php/**/*.php"
   ]
 };
+
 // Gulp Tasks:
 function clean() {
   return del("dist");
 }
+
 function includeHtml() {
   return gulp
     .src("src/html/*.html")
@@ -51,6 +59,7 @@ function includeHtml() {
     .pipe(formatHtml())
     .pipe(gulp.dest("dist"));
 }
+
 function style() {
   return gulp
     .src("src/styles/styles.less")
@@ -69,6 +78,7 @@ function style() {
     .pipe(rename("styles.min.css"))
     .pipe(gulp.dest("dist/styles"));
 }
+
 function js() {
   return gulp
     .src("src/scripts/dev/*.js")
@@ -88,12 +98,14 @@ function js() {
     )
     .pipe(gulp.dest("dist/scripts"));
 }
+
 function jsCopy() {
   return gulp
     .src(resources.jsVendor)
     .pipe(plumber())
     .pipe(gulp.dest("dist/scripts"));
 }
+
 function copy() {
   return gulp
     .src(resources.static, {
@@ -101,6 +113,7 @@ function copy() {
     })
     .pipe(gulp.dest("dist/"));
 }
+
 function images() {
   return gulp
     .src(resources.images)
@@ -113,6 +126,7 @@ function images() {
     )
     .pipe(gulp.dest("dist/assets/images"));
 }
+
 function svgSprite() {
   return gulp
     .src(resources.svgSprite)
@@ -131,6 +145,7 @@ function svgSprite() {
     .pipe(rename("symbols.svg"))
     .pipe(gulp.dest("dist/assets/icons"));
 }
+
 const build = gulp.series(
   clean,
   copy,
@@ -141,10 +156,12 @@ const build = gulp.series(
   images,
   svgSprite
 );
+
 function reloadServer(done) {
   server.reload();
   done();
 }
+
 function serve() {
   server.init({
     server: "dist"
@@ -157,6 +174,7 @@ function serve() {
   gulp.watch(resources.images, { delay: 500 }, gulp.series(images, reloadServer));
   gulp.watch(resources.svgSprite, gulp.series(svgSprite, reloadServer));
 }
+
 const start = gulp.series(build, serve);
 export {
   clean,
